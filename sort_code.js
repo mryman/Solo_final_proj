d57df16c31577b951743c535c9a416538622fd8c
mryman <mark.ryman@gmail.com>
2015-01-24 17:55:30 -0800

    begins comparison in insertionsort method.  Stuck on how to state the line to return to outer loop.

--- sort_code.js
+++ sort_code.js
@@ -2,27 +2,56 @@ $(function() {
   function Donor(person, dollars) {
     this.person = person;
     this.dollars = dollars;
-}
+  }
 
-Donor.prototype.toHtml = function() {
-return "<li class='donors'>" + "Donor" + this.person + "-" + "$" + this.dollars + "</li>";
-}
+  Donor.prototype.toHtml = function() {
+  return "<li class='donors' data-pledge='"+ this.dollars +"'>" + "Donor" + this.person + "-" + "$" + this.dollars + "</li>";
+  }
 
-function DonorSet() {
-  thisDonorSet = this;
-  this.donorNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  this.pledge = [20, 35, 100, 500];
-  $.each(thisDonorSet.donorNum, function() {
-    var donorNum = this;
-      var pledge = thisDonorSet.pledge[Math.floor(Math.random() * 4)];
-      var donor = new Donor(donorNum, pledge);
-      $("#donors").append(donor.toHtml());
-  });
-}
+  function DonorSet() {
+    thisDonorSet = this;
+    this.donorNum = [1, 2, 3, 4];
+    this.pledge = [10, 20, 35, 50, 100, 500, 1000];
+    $.each(thisDonorSet.donorNum, function() {
+      var donorNum = this;
+        var pledge = thisDonorSet.pledge[Math.floor(Math.random() * 7)];
+        var donor = new Donor(donorNum, pledge);
+        $("#donors").append(donor.toHtml());
+    });
+  }
 
-//var randomNum = Math.floor((Math.random() * 4) + 1); //Use this for array index to get dollars out of array.  Drop the + 1 so numbers are always between 0 and 3?
+  function insertionSort() {
+    var len     = $("li").length,     // number of items in the array
+      value,                      // the value currently being compared
+      i,                          // index into unsorted section
+      j;                          // index into sorted section
 
-donorSet = new DonorSet();
+
+    for (i=0; i < len; i++) {
+        // store the current value because it may shift later
+
+      value = $("li").eq(i).attr("data-pledge");
+      console.log(value);
+
+        /*
+         * Whenever the value in the sorted section is greater than the value
+         * in the unsorted section, shift all items in the sorted section over
+         * by one. This creates space in which to insert the value.
+         */
+      for (j=i-1; j > -1 && $("li").eq(j).attr("data-pledge") > value; j--) {
+        $("li").eq(j).insertAfter($("li").eq(i));
+
+      }
+
+
+    }
+
+    //return $("li");
+  }
+
+  donorSet = new DonorSet();
+  $("button").on("click", insertionSort);
+
 });
 
 
@@ -38,37 +67,3 @@ donorSet = new DonorSet();
 
 
 
-
-
-
-
-
-
-/* Apply this to DOM items after they are successfully showing up on the screen
-function insertionSort(items) {
-
-    var len     = items.length,     // number of items in the array
-        value,                      // the value currently being compared
-        i,                          // index into unsorted section
-        j;                          // index into sorted section
-
-    for (i=0; i < len; i++) {
-
-        // store the current value because it may shift later
-        value = items[i];
-
-        /*
-         * Whenever the value in the sorted section is greater than the value
-         * in the unsorted section, shift all items in the sorted section over
-         * by one. This creates space in which to insert the value.
-
-        for (j=i-1; j > -1 && items[j] > value; j--) {
-            items[j+1] = items[j];
-        }
-
-        items[j+1] = value;
-    }
-
-    return items;
-}
-*/
